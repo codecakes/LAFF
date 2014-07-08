ee9e1f1a0adc622c782f6f5c4c35ddc2023880d5
Akul <akurnya@gmail.com>
2014-07-08 20:09:26 +0530

    changed is everywhere to ==

--- laff/matvec/gemv.py
+++ laff/matvec/gemv.py
@@ -17,7 +17,7 @@ def gemv(trans, alpha, A, x, beta, y):
 
     if(type(alpha) is matrix):
         m_alpha, n_alpha = alpha.shape
-        assert m_alpha is 1 and n_alpha is 1, "laff.gemv: alpha is neither a scalar nor a 1 x 1 matrix"
+        assert m_alpha == 1 and n_alpha == 1, "laff.gemv: alpha is neither a scalar nor a 1 x 1 matrix"
         scalar_alpha = alpha[0,0]
     else:
         assert isinstance( alpha, (int,float,complex)), "laff.gemv: alpha is neither a scalar nor a 1 x 1 matrix"
@@ -31,7 +31,7 @@ def gemv(trans, alpha, A, x, beta, y):
 
     if(type(beta) is matrix):
         m_beta, n_beta = beta.shape
-        assert m_beta is 1 and n_beta is 1, "laff.gemv: beta is neither a scalar nor a 1 x 1 matrix"
+        assert m_beta == 1 and n_beta == 1, "laff.gemv: beta is neither a scalar nor a 1 x 1 matrix"
         scalar_beta = beta[0,0]
     else:
         assert isinstance( beta, (int,float,complex)), "laff.gemv: beta is neither a scalar nor a 1 x 1 matrix"
@@ -51,46 +51,46 @@ def gemv(trans, alpha, A, x, beta, y):
     m_A, n_A = A.shape
 
 
-    assert m_x is 1 or n_x is 1, "laff.gemv: x is not a vector"
-    assert m_y is 1 or n_y is 1, "laff.axpy: y is not a vector"
+    assert m_x == 1 or n_x == 1, "laff.gemv: x is not a vector"
+    assert m_y == 1 or n_y == 1, "laff.axpy: y is not a vector"
 
     if 'No transpose' == trans:
-        if m_x is 1 and m_y is 1: # x is a row, y is a row
+        if m_x == 1 and m_y == 1: # x is a row, y is a row
             assert n_y == m_A, "laff.gemv: size mismatch between y and A"
             assert n_x == n_A, "laff.gemv: size mismatch between x and A"
             y[0,:] = scalar_alpha * x * transpose( A ) + scalar_beta * y[0,:];
             
-        elif n_x is 1 and n_y is 1: # x is a column, y is a column
+        elif n_x == 1 and n_y == 1: # x is a column, y is a column
             assert m_y == m_A, "laff.gemv: size mismatch between y and A"
             assert m_x == n_A, "laff.gemv: size mismatch between x and A"
             y[:,0] = scalar_alpha * A * x + scalar_beta * y[:,0]
 
-        elif m_x is 1 and n_y is 1: # x is a row, y is a column
+        elif m_x == 1 and n_y == 1: # x is a row, y is a column
             assert m_y == m_A, "laff.gemv: size mismatch between y and A"
             assert n_x == n_A, "laff.gemv: size mismatch between x and A"
             y[:,0] = scalar_alpha * A * transpose( x ) + scalar_beta * y[:,0]
 
-        elif n_x is 1 and m_y is 1: # x is a column, y is a row
+        elif n_x == 1 and m_y == 1: # x is a column, y is a row
             assert n_y == m_A, "laff.gemv: size mismatch between y and A"
             assert m_x == n_A, "laff.gemv: size mismatch between x and A"
             y[0,:] = scalar_alpha * transpose( x ) * transpose( A ) + scalar_beta * y[0,:]
     else:
-        if m_x is 1 and m_y is 1: # x is a row, y is a row
+        if m_x == 1 and m_y == 1: # x is a row, y is a row
             assert n_y == n_A, "laff.gemv: size mismatch between y and A"
             assert n_x == m_A, "laff.gemv: size mismatch between x and A"
             y[0,:] = scalar_alpha *  x  *  A  + scalar_beta * y[0,:]
             
-        elif n_x is 1 and n_y is 1: # x is a column, y is a column
+        elif n_x == 1 and n_y == 1: # x is a column, y is a column
             assert m_y == n_A, "laff.gemv: size mismatch between y and A"
             assert m_x == m_A, "laff.gemv: size mismatch between x and A"
             y[:,0] = scalar_alpha * transpose( A ) * x + scalar_beta * y[:,0]
 
-        elif m_x is 1 and n_y is 1: # x is a row, y is a column
+        elif m_x == 1 and n_y == 1: # x is a row, y is a column
             assert m_y == n_A, "laff.gemv: size mismatch between y and A"
             assert n_x == m_A, "laff.gemv: size mismatch between x and A"
             y[:,0] = scalar_alpha * transpose( A ) * transpose( x ) + scalar_beta * y[:,0]
 
-        elif n_x is 1 and m_y is 1: # x is a column, y is a row
+        elif n_x == 1 and m_y == 1: # x is a column, y is a row
             assert n_y == n_A, "laff.gemv: size mismatch between y and A"
             assert m_x == m_A, "laff.gemv: size mismatch between x and A"
             y[0,:] = scalar_alpha * transpose( x ) * A + scalar_beta * y[0,:]
